5e3d4d3d85b6ca6f15165f094e9a84624c238977
Gaab shiine <gaabshiine@gmail.com>
2024-08-22 20:55:20 +0300

    all_in_one

--- admin_page_app/js/app.js
+++ admin_page_app/js/app.js
@@ -1318,20 +1318,3 @@ function ($) {
 
 
     }(window.jQuery);
-
-
-    document.addEventListener("DOMContentLoaded", function() {
-        document.querySelectorAll('form').forEach(form => {
-            form.addEventListener('submit', function() {
-                const button = form.querySelector('button[type="submit"]');
-                const spinner = button.querySelector('.spinner-border');
-                const buttonText = button.querySelector('.button-text');
-                
-                spinner.classList.remove('d-none');
-                buttonText.textContent = 'Processing...';
-                button.disabled = true;
-            });
-        });
-    });
-    
-    
